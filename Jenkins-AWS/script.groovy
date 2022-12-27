b643ddd1c94c79532befeb6acaa236d4690ed300
Prashant Misra <98226143+Prashant2623@users.noreply.github.com>
2022-12-27 09:33:12 +0530

    Create script.groovy

---
+++ Jenkins-AWS/script.groovy
@@ -0,0 +1,17 @@
+def buildApp() {
+
+    echo "Building the application..."
+    echo "building version ${env.NEW_VERSION}"
+}
+return this
+
+def testApp() {
+    echo "Testing the application..."
+}
+return this
+
+def deployApp() {
+     echo "Deploying the application..."
+         withCredentials([usernamePassword(credentialsId: 'server-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+}                
+}
