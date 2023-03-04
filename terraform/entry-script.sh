53d4a16ffc33c0ced56ded99f95b68c31ba9b0b4
Prashant Misra <98226143+Prashant2623@users.noreply.github.com>
2023-03-04 11:29:32 +0530

    Update and rename entry-script.tpl to entry-script.sh

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,3 +1,5 @@
+# ! /bin/bash
 sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
-sudo usermod -aG docker ec2-user
+sudo usermod  -aG  docker ec2-user
+docker run -p 8080:80 nginx
