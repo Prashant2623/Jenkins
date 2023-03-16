6a9b9e8959e53ebfacaa807aedf4d7d348308ea8
Prashant Misra <98226143+Prashant2623@users.noreply.github.com>
2023-03-16 23:03:01 +0530

    Update entry-script.sh

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -7,6 +7,6 @@ sudo usermod  -aG  docker ec2-user
 
 # Docker login
 export DOCKER_USER=$1
-export DOCKER_USER=$2
+export DOCKER_PWD=$2
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 echo "success"
